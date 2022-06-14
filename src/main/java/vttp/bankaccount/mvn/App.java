4b1f5ec438a5a4f4f20d5919020d49929afb4350
drewkwan <andrewtimkwan@gmail.com>
2022-06-14 12:42:20 +0800

    first commit

---
+++ src/main/java/vttp/bankaccount/mvn/App.java
@@ -0,0 +1,13 @@
+package vttp.bankaccount.mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
