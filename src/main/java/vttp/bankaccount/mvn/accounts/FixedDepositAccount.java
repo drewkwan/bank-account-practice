4cc95fd62b59642fcfa708c27720bc945e54fc86
drewkwan <andrewtimkwan@gmail.com>
2022-06-14 17:22:10 +0800

    Added in FixedDeposit override

--- src/main/java/vttp/bankaccount/mvn/accounts/FixedDepositAccount.java
+++ src/main/java/vttp/bankaccount/mvn/accounts/FixedDepositAccount.java
@@ -10,6 +10,7 @@ package vttp.bankaccount.mvn.accounts;
 public class FixedDepositAccount extends BankAccount {
     private float interest = 3;
     private int duration = 6;
+   
     private int durationInterestCounter = 0;
 
     //Overload the bankaccount constructors
@@ -31,5 +32,61 @@ public class FixedDepositAccount extends BankAccount {
         this.duration = duration;
         this.durationInterestCounter = 0;
     }
-    
+
+    //setters and getters
+
+     public float getInterest() {
+        return interest;
+    }
+
+    private void setInterest(float interest) {
+        if(interest <0) {
+            throw new IllegalArgumentException("Unsupported interest");
+        }
+
+        this.interest = interest;
+        //set back to default if invalid amount entered
+    }
+
+    public int getDuration() {
+        return duration;
+        
+    }
+
+    public void setDuration(int duration) {
+        if (duration<0) {
+            throw new IllegalArgumentException("Unsupported duration");
+        }
+        this.duration = duration;
+    }
+
+    public int getDurationInterestCounter() {
+        return durationInterestCounter;
+    }
+
+    public void setDurationInterestCounter(int durationInterestCounter) {
+        if (durationInterestCounter == 1) {
+            throw new IllegalArgumentException("Duration and Interest can only be changed once.");
+        }
+        setInterest(interest);
+        setDuration(duration);
+        durationInterestCounter++;
+    }
+
+    @Override
+    public float withdraw (String withdrawAmt) {
+        //NOP
+        return 0;
+    }
+
+    @Override
+    public Float deposit (String depositAmt) {
+        return 0f;
+    }
+
+    @Override
+    public float getBalance() {
+        return super.getBalance() +interest;
+    }
+
 }
